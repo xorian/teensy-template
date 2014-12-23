17e239b519b55ed2a15d2e327a2fae415ca00850
Laurentiu Nicola <lnicola@dend.ro>
2014-12-23 22:25:28 +0200

    Remove strlen() since it's already present in stdlib

--- teensy3/nonstd.c
+++ teensy3/nonstd.c
@@ -32,15 +32,6 @@
 #include <string.h>
 #include <stdio.h>
 
-size_t strlen(const char *s)
-{
-	size_t n=0;
-
-	while (*s++) n++;
-	return n;
-}
-
-
 char * ultoa(unsigned long val, char *buf, int radix) 	
 {
 	unsigned digit;
